dc9d3c025c2671dc537689dbf8d38de1299cfffe
eddieogola <edwineogola@gmail.com>
2025-10-29 01:26:13 +0300

    Add Go service configuration and implementation for AI model

--- go-ai/main.go
+++ go-ai/main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/openai/openai-go/v3"
 	"github.com/openai/openai-go/v3/option"
 )
 
 func main() {
+	baseUrl := os.Getenv("BASE_URL")
+	modelName := os.Getenv("MODEL_NAME")
+
+	if baseUrl == "" || modelName == "" {
+		panic("BASE_URL and MODEL_NAME must be set")
+	}
+
 	client := openai.NewClient(
 		option.WithAPIKey("dummyapikey"),
-		option.WithBaseURL("http://localhost:12434/engines/llama.cpp/v1"),
+		option.WithBaseURL(baseUrl),
 	)
 	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
 		Messages: []openai.ChatCompletionMessageParamUnion{
 			openai.SystemMessage("You are a helpful assistant"),
 			openai.UserMessage("Write a haiku about recursion in programming."),
 		},
-		Model: "ai/gemma3",
+		Model: modelName,
 	})
 	if err != nil {
 		panic(err.Error())
